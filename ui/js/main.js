0e58b4e42bfe3f710adbcd1a1a7ccebfc62feaca
GOPALAKRISHNAN SANKARANARAYANAN <gopalakrishnan.sankaranarayanan@msc.com>
2018-05-27 11:35:42 +0530

    update

--- ui/js/main.js
+++ ui/js/main.js
@@ -1,15 +1,41 @@
-Vue.component('menu-item', {
-    props:['menuitem'],
-    template: '<li><a :href="menuitem.url">{{ menuitem.text }}</a></li>'
-  })
-var app = new Vue({
-  el: '#kjsapp',
-  data: {
-    name: 'Gopala Krishnan S',
-    menu:[{ url:'about' , text:"About"},
-          { url:'work' , text:"Work"},
-          { url:'contact' , text:"Contact"}]
-  }
-});
-
-var slide = new DoSlide('.wrapper');
+(function () {
+  var kjs = {
+    app: null,
+    slider: null,
+    addComponents: function () {
+      Vue.component('menu-item', {
+        props: ['menuitem'],
+        template: '<li><a :href="menuitem.url">{{ menuitem.text }}</a></li>'
+      });
+    },
+    animate: function (selector) {
+      var elm = document.querySelector(selector);
+      var newone = elm.cloneNode(true);
+      elm.parentNode.replaceChild(newone, elm);
+    },
+    init: function () {
+      this.addComponents();
+      this.app = new Vue({
+        el: '#kjsapp',
+        data: {
+          name: 'Gopala Krishnan S',
+          menu: [{
+              url: 'about',
+              text: "About"
+            },
+            {
+              url: 'work',
+              text: "Work"
+            },
+            {
+              url: 'contact',
+              text: "Contact"
+            }
+          ]
+        }
+      });
+      this.slider = new DoSlide('.wrapper');
+    },
+  };
+  kjs.init();
+})();
